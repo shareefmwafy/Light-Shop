562d9d690343f4e98e5f0a23b61983e6c8eaa327
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-04-02 20:30:33 +0300

    Create Category Validation

--- Light_Shp.API/DTOs/Requests/CategoryRequest.cs
+++ Light_Shp.API/DTOs/Requests/CategoryRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace Light_Shop.API.DTOs.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Light_Shop.API.DTOs.Requests
 {
     public class CategoryRequest
     {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
         public string Name { get; set; }
         public string Description { get; set; }
         public bool Status { get; set; }

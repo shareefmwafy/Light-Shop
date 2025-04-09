f01e5e6303c229870e6baa3f8b08dcb09d2ac1e5
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-04-09 05:51:25 +0300

    Create Application User & Begin with Identity

---
+++ Light_Shp.API/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Light_Shop.API.Models
+{
+
+    public enum ApplicationUserGender
+    {
+        Male,
+        Female
+    }
+    public class ApplicationUser : IdentityUser
+    {
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ApplicationUserGender Gender { get; set; }
+    }
+}

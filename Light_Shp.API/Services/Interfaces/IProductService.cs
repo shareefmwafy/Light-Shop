39a2a741f1ab47035983fbb749cad3e31209052a
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-04-09 05:34:14 +0300

    Create Products Pagination

--- Light_Shp.API/Services/Interfaces/IProductService.cs
+++ Light_Shp.API/Services/Interfaces/IProductService.cs
@@ -6,10 +6,10 @@ namespace Light_Shop.API.Services.Interfaces
 {
     public interface IProductService
     {
-        IEnumerable<Product> GetAll();
+        IEnumerable<Product> GetAll(string? query, int page, int limit);
         Product? Get(Expression<Func<Product, bool>> expression);
         Product Add(ProductRequest productRequest);
-        bool Edit(int id, ProductRequest productRequest);
+        bool Edit(int id, ProductUpdateRequest productRequest);
         bool Remove(int id);
     }
 }
--- Light_Shp.API/obj/Debug/net9.0/Light_Shop.API.AssemblyInfo.cs
+++ Light_Shp.API/obj/Debug/net9.0/Light_Shop.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Light_Shop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aaaddfcd9b332f28725ad74a73f835c45e0d92f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d200b06057b4a395b090155501794fe1b268f45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Light_Shop.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Light_Shop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

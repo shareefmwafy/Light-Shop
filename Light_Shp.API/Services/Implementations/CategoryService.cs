6ab9331c1bba8cd8f3ccf2100f40740e6f4b079b
Shareef Mwafy <sh.mwafy1@gmail.com>
2025-04-19 11:46:50 +0300

    Create Cancellation With Async for Category methods

--- Light_Shp.API/Services/Implementations/CategoryService.cs
+++ Light_Shp.API/Services/Implementations/CategoryService.cs
@@ -14,21 +14,28 @@ namespace Light_Shop.API.Services.Implementations
         {
             this.context = context;
         }
-        public async Task<Category> Add(Category category, CancellationToken cancellationToken = default)
+        public async Task<Category> AddAsync(Category category, CancellationToken cancellationToken = default)
         {
             await context.Categories.AddAsync(category, cancellationToken);
             await context.SaveChangesAsync();
             return category;
         }
 
-        public bool Edit(int id, Category category)
+        public async Task<bool> EditAsync(int id, Category category, CancellationToken cancellationToken)
         {
-            Category? categoryInDb = context.Categories.AsNoTracking().FirstOrDefault(c => c.Id == id);
+            Category? categoryInDb = context.Categories.Find(id);
             if (categoryInDb == null) return false;
-
-            category.Id = id;
-            context.Categories.Update(category);
-            context.SaveChanges();
+            categoryInDb.Name = category.Name;
+            categoryInDb.Description = category.Description;
+            await context.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+        public async Task<bool> UpdateStatusAsync(int id, Category category, CancellationToken cancellationToken)
+        {
+            Category? categoryInDb = context.Categories.Find(id);
+            if (categoryInDb == null) return false;
+            categoryInDb.Status = !categoryInDb.Status;
+            await context.SaveChangesAsync(cancellationToken);
             return true;
         }
 
@@ -42,13 +49,13 @@ namespace Light_Shop.API.Services.Implementations
             return context.Categories.ToList();
         }
 
-        public bool Remove(int id)
+        public async Task<bool> RemoveAsync(int id, CancellationToken cancellationToken)
         {
             Category? categoryInDb = context.Categories.Find(id);
             if (categoryInDb == null) return false;
 
             context.Categories.Remove(categoryInDb);
-            context.SaveChanges();
+            await context.SaveChangesAsync(cancellationToken);
             return true;
         }
     }
